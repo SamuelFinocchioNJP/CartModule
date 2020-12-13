8f061228a9248374503313ca669383d9a84f221d
SamuelFinocchioNJP <samuelfinocchioit@gmail.com>
2020-12-13 17:29:58 +0100

    Added tests, and changed Set to Array

--- src/cart.ts
+++ src/cart.ts
@@ -2,46 +2,44 @@ import { ICart } from './interfaces/cart.prototype';
 import { PriceableAddition } from './models/priceable-addition.model';
 import { PriceableItem } from './models/priceable-item.model';
 
-export class Cart<T extends PriceableItem> implements ICart<T> {
-	items: Map<string, T>;
+export class Cart<T extends PriceableItem<PriceableAddition>> implements ICart<T> {
+	items: Array<T>;
 	subtotal: number;
 
 	constructor() {
-		this.items = new Map<string, T>();
+		this.items = new Array<T>();
 		this.subtotal = 0;
 	}
 
 	private getAdditionsCost(additions: Array<PriceableAddition>): number {
 		return additions
-			.map((addition: PriceableAddition) => addition.price)
+			.map(
+				(addition: PriceableAddition) =>
+					addition.price * (addition.amount ?? 1)
+			)
 			.reduce(
 				(accumulator: number, current: number) => accumulator + current
 			);
 	}
 
-	addToCart(key: string, item: T): boolean {
-		/// If item exist already
-		if (this.items.has(key)) {
-			return false;
-		}
-
+	addToCart(item: T): boolean {
 		/// Adds item and it's price
-		if (this.items.set(key, item)) {
-			this.subtotal += item.price * item.amount;
+		const currentItemsAmount: number = this.items.length;
+		if (this.items.push(item) === currentItemsAmount + 1) {
+			this.subtotal += item.price;
 			if (item.additions) {
-				this.subtotal += this.getAdditionsCost([
-					...item.additions.values(),
-				]);
+				this.subtotal += this.getAdditionsCost(item.additions);
 			}
+			/// Returns true if item has been successfully inserted
+			return true;
 		}
 
-		/// Returns true if item has been successfully inserted
-		return this.items.has(key);
+		return false;
 	}
 
-	removeFromCart(key: string): boolean {
+	removeFromCart(index: number): boolean {
 		/// Retrieves item
-		const item: T | undefined = this.items.get(key);
+		const item: T | undefined = this.items[index];
 
 		/// If item did NOT exist
 		if (item === undefined) {
@@ -49,22 +47,21 @@ export class Cart<T extends PriceableItem> implements ICart<T> {
 		}
 
 		/// Attempts deletion
-		if (this.items.delete(key)) {
+		if (this.items.splice(index, 1) !== []) {
 			/// Scales subtotal
-			this.subtotal -= item.price * item.amount;
+			this.subtotal -= item.price;
+
+			/// Scales additions price
+			if (item.additions) {
+				this.subtotal -= this.getAdditionsCost(item.additions);
+			}
+			return true;
 		}
 
-		/// Returns true if item no longer exists
-		return !this.items.has(key);
+		return false;
 	}
 
-	checkout(cb: (items: Map<string, T>) => unknown): Cart<T> {
-		/// Callback with a SHALLOW COPY of the cart
-		cb({ ...this.items });
-		return { ...this };
-	}
-
-	toArray(): Array<T> {
-		return [...this.items.values()];
+	checkout(cb: (items: Array<T>, subtotal: number) => unknown): unknown {
+		return cb([...this.items], this.subtotal);
 	}
 }
--- src/interfaces/cart.prototype.ts
+++ src/interfaces/cart.prototype.ts
@@ -1,11 +1,11 @@
+import { PriceableAddition } from '../models/priceable-addition.model';
 import { PriceableItem } from '../models/priceable-item.model';
 
-export interface ICart<T extends PriceableItem> {
-	items: Map<string, T>;
+export interface ICart<T extends PriceableItem<PriceableAddition>> {
+	items: Array<T>;
 	subtotal: number;
 
-	addToCart(key: string, item: T): boolean;
-	removeFromCart(key: string): boolean;
-	checkout(cb: (items: Map<string, T>) => unknown): Array<T> | unknown;
-	toArray(): Array<T>;
+	addToCart(item: T): boolean;
+	removeFromCart(index: number): boolean;
+	checkout(cb: (items: Array<T>, subtotal: number) => unknown): unknown;
 }
